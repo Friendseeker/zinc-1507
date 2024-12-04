aaaebd189a96236a50ab913dd5434f06e5983dae
friendseeker <66892505+Friendseeker@users.noreply.github.com>
2024-12-03 19:59:31 -0800

    Minor simplification

--- A/src/main/scala/Refined.scala
+++ A/src/main/scala/Refined.scala
@@ -1,12 +1,11 @@
 package A
 
 class SRC[_]
-class Filter
 class Refined {
-  def select(cols: SRC[_]*) = new {
-    def where(filter: Filter => Filter)(implicit d: DummyImplicit) = {
+  def select() = new {
+    def where() = {
       new {
-        def using(opt: Option[SRC[_]] => Some[SRC[_]])(implicit d: DummyImplicit) = opt
+        def using(opt: Option[SRC[_]] => Some[SRC[_]]) = opt
       }
     }
   }
